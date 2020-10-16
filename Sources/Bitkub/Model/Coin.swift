ddd9e54c830292a59193c67aff68497ca0866d5e
Marcio Costa Silva <marcio.costa.silva@gmail.com>
2020-10-17 00:54:58 +0700

    Add models

---
+++ Sources/Bitkub/Model/Coin.swift
@@ -0,0 +1,66 @@
+//
+//  Coin.swift
+//  
+//
+//  Created by Marcio on 10/17/20.
+//
+
+import Foundation
+
+struct Coin: Decodable, Identifiable {
+	var name: String {
+		self.symbol?.name ?? CoinSymbol.UNKNOWN.rawValue
+	}
+	let id: Int
+	let symbol: CoinSymbol?
+	let last: Double
+	let percentChange: Double
+	var favorite: Bool
+	var lowestAsk: Double = 0
+	var highestBid: Double = 0
+	var high24hr: Double = 0
+	var low24hr: Double = 0
+
+	var imageName: String {
+		guard let symbol = self.symbol?.rawValue else {
+			return ""
+		}
+		return symbol.replacingOccurrences(of: "THB_", with: "")
+	}
+
+	enum CodingKeys: String, CodingKey {
+		case id
+		case symbol
+		case last
+		case percentChange
+		case lowestAsk
+		case highestBid
+		case high24hr
+	}
+
+	init(from decoder: Decoder) throws {
+		let container = try decoder.container(keyedBy: CodingKeys.self)
+
+		id = try container.decode(Int.self, forKey: CodingKeys.id)
+		last = try container.decode(Double.self, forKey: CodingKeys.last)
+		percentChange = try container.decode(Double.self, forKey: CodingKeys.percentChange)
+		symbol = CoinSymbol(rawValue: container.codingPath.first!.stringValue)
+		lowestAsk = try container.decode(Double.self, forKey: CodingKeys.lowestAsk)
+		highestBid = try container.decode(Double.self, forKey: CodingKeys.highestBid)
+//		favorite = PreferencesManager.favorites().contains(id)
+		high24hr = try container.decode(Double.self, forKey: CodingKeys.high24hr)
+	}
+
+
+	init(id: Int, symbol: CoinSymbol, last: Double, percentChange: Double = 0, favorite: Bool = false) {
+		self.id = id
+		self.symbol = symbol
+		self.last = last
+		self.percentChange = percentChange
+		self.favorite = favorite
+	}
+
+	mutating func refreshFavorite() {
+//		favorite = PreferencesManager.favorites().contains(id)
+	}
+}
---
+++ Sources/Bitkub/Model/CoinSymbol.swift
@@ -0,0 +1,94 @@
+//
+//  CoinSymbol.swift
+//  
+//
+//  Created by Marcio on 10/17/20.
+//
+
+import Foundation
+
+public enum CoinSymbol: String, Codable {
+	case THB_BTC, THB_ETH, THB_WAN, THB_ADA, THB_OMG, THB_BCH, THB_USDT, THB_LTC, THB_XRP, THB_BSV, THB_ZIL, THB_SNT, THB_CVC, THB_LINK, THB_GNT, THB_IOST, THB_ZRX, THB_KNC, THB_ENG, THB_RDN, THB_ABT, THB_MANA, THB_INF, THB_CTXC, THB_XLM, THB_SIX, THB_JFIN, THB_EVX, THB_BNB, THB_POW, THB_DOGE, THB_DAI, THB_BAND, THB_KSM, THB_DOT, UNKNOWN, THB_THB
+	static var allValues: [CoinSymbol] {
+		[.THB_BTC, .THB_ETH, .THB_WAN, .THB_ADA, .THB_OMG, .THB_BCH, .THB_USDT, .THB_LTC, .THB_XRP, .THB_BSV, .THB_ZIL, .THB_SNT, .THB_CVC, .THB_LINK, .THB_GNT, .THB_IOST, .THB_ZRX, .THB_KNC, .THB_ENG, .THB_RDN, .THB_ABT, .THB_MANA, .THB_INF, .THB_CTXC, .THB_XLM, .THB_SIX, .THB_JFIN, .THB_EVX, .THB_BNB, .THB_POW, .THB_DOGE, .THB_DAI, .THB_BAND, .THB_KSM, .THB_DOT]
+	}
+
+	public var name: String {
+		switch self {
+		case .THB_THB:
+			return "THB"
+		case .THB_BTC:
+		  return "Bitcoin"
+		case .THB_ETH:
+		  return "Ethereum"
+		case .THB_WAN:
+		  return "Wancoin"
+		case .THB_ADA:
+		  return "Cardano"
+		case .THB_OMG:
+		  return "OmiseGO"
+		case .THB_BCH:
+		  return "Bitcoin Cash"
+		case .THB_USDT:
+		  return "Tether"
+		case .THB_LTC:
+		  return "Litecoin"
+		case .THB_XRP:
+		  return "XRP"
+		case .THB_BSV:
+		  return "Bitcoin SV"
+		case .THB_ZIL:
+		  return "Zilliqa"
+		case .THB_SNT:
+		  return "StatusNetwork"
+		case .THB_CVC:
+		  return "Civic"
+		case .THB_LINK:
+		  return "ChainLink Token"
+		case .THB_GNT:
+		  return "Golem"
+		case .THB_IOST:
+		  return "IOSToken"
+		case .THB_ZRX:
+		  return "0x"
+		case .THB_KNC:
+		  return "KyberNetwork"
+		case .THB_ENG:
+		  return "Enigma"
+		case .THB_RDN:
+		  return "Raiden Network Token"
+		case .THB_ABT:
+		  return "Arcblock"
+		case .THB_MANA:
+		  return "Decentraland"
+		case .THB_INF:
+		  return "InfinitusTokens"
+		case .THB_CTXC:
+		  return "Cortex Coin"
+		case .THB_XLM:
+		  return "Stellar"
+		case .THB_SIX:
+		  return "Six"
+		case .THB_JFIN:
+		  return "JFIN Coin"
+		case .THB_EVX:
+		  return "Everex"
+		case .THB_BNB:
+		  return "Binance's BNB Token"
+		case .THB_POW:
+		  return "Power Ledger"
+		case .THB_DOGE:
+		  return "Dogecoin"
+		case .THB_DAI:
+		  return "Dai Stablecoin"
+		case .THB_BAND:
+		  return "Band"
+		case .THB_KSM:
+		  return "Kusama"
+		case .THB_DOT:
+		  return "Polkadot"
+		case .UNKNOWN:
+			return "Unknown"
+		}
+	}
+}
