8f540eefa36146c7ab91ae1023790338af5a67ab
Marcio Costa Silva <marcio@hey.com>
2020-10-17 21:53:54 +0700

    Add tests to load balances

--- Tests/BitkubTests/BitkubTests.swift
+++ Tests/BitkubTests/BitkubTests.swift
@@ -16,6 +16,21 @@ final class BitkubTests: XCTestCase {
 		wait(for: [expectation], timeout: 5)
 	}
 
+	func testLoadBalance() {
+		let key = ProcessInfo.processInfo.environment["KEY"]
+		let secret = ProcessInfo.processInfo.environment["SECRET"]
+		XCTAssertNotNil(key)
+		XCTAssertNotNil(secret)
+		let controller = BitkubController()
+		let expectation = XCTestExpectation(description: "Balance retrieved from Bitkub")
+		controller.loadBalance(key: key!, secret: secret!)
+		controller.$balances.drop(while: {$0.count == 0}).sink { (balances) in
+			expectation.fulfill()
+		}
+		.store(in: &cancellables)
+		wait(for: [expectation], timeout: 5)
+	}
+
     static var allTests = [
         ("testLoadCoins", testLoadCoins),
     ]
